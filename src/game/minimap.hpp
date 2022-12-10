7b3ad042beb77f5dcc412bc215fbefdd44145b8e
hazardland <hazardland@gmail.com>
2022-12-10 23:50:12 +0400

    minimap working with generateTerrain added noise story

--- src/game/minimap.hpp
+++ src/game/minimap.hpp
@@ -9,6 +9,7 @@ using namespace std;
 
 #include <game/object.hpp>
 #include <game/state.hpp>
+#include <game/noise.h>
 
 class Minimap: public Object {
 
@@ -56,8 +57,8 @@ class Minimap: public Object {
     }
     void setPixel(int x, int y, int color[3]) {
         SDL_Rect rect;
-        rect.x = x;
-        rect.y = y;
+        rect.x = x*scale;
+        rect.y = y*scale;
         rect.h = scale;
         rect.w = scale;
         SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, color[0], color[1], color[2]));
@@ -82,19 +83,50 @@ class Minimap: public Object {
     //     modified = true;
     // }
 
-
-
     void updateSurface() {
         texture = SDL_CreateTextureFromSurface(renderer, surface);
         modified = false;
     }
+
     virtual void render(State* state) {
         if (modified) {
             updateSurface();
         }
         SDL_RenderCopy(renderer, texture, &frame, &position);
     }
-    
+
+    /**
+     * Generates terrain using Open Simplex Noise algorithm
+     * @param seed Noise seed
+     * @param intensity Noise intensity. ex.: 0.01 (Lower is smoother higher is intense)
+     * @param variations Terrain variation count. ex.: 3 (1:Water, 2:Grass, 3:Trees)
+     * @param ranges Terrain ranges per variation between ex.: {0.3, 0.5, 1} (0 - 0.3: Water, 0.3 - 0.5: Grass, 0.5 - 1: Trees)
+     * @param colors Terrain variation rbg colors ex {{0,255,255}, {76,30,0}, {15,255,0}}
+    */
+    void generateTerrain(
+                 int seed, 
+                 float intensity,
+                 int variations,
+                 float ranges[],
+                 int colors[][3]) {
+
+        OpenSimplexNoise::Noise noise{seed};
+        for (int y=0; y<width; y++) {
+            for (int x=0; x<height; x++)
+            {
+                // alpha = (noise.eval(x*0.01, y*0.01) + 1) / 2.0  * 255.0;
+                // minimap->setPixel(x, y, 255, 255, 255, alpha);       
+                float height = (noise.eval(x*intensity, y*intensity) + 1) / 2.0;
+                for (int i=0; i<variations; i++){
+                    if (height<=ranges[i]) {
+                        setPixel(x, y, colors[i]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
 };
 
 #endif
--- src/test_scene_perlin.hpp
+++ src/test_scene_perlin.hpp
@@ -1,70 +1,46 @@
 #ifndef TEST_SCENE_PERLIN
 #define TEST_SCENE_PERLIN
 
-using namespace std;
-#include <iostream>
-#include <map>
-#include <vector>
-
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-
 #include <game/scene.hpp>
 #include <game/minimap.hpp>
-#include <game/noise.h>
+
+// We will have 6 terrain types
+int TERRAIN_COLORS[6][3] = {
+    {51, 51, 255},
+    {0, 0, 255},
+    {0, 153, 0},
+    {255, 153, 51},
+    {96, 96, 96},
+    {255, 255, 255}
+};
+
+// And we declare noise range per terrain type
+float TERRAIN_RANGES[6] = {
+    0.3,
+    0.35,
+    0.5,
+    0.7,
+    0.9,
+    1
+};
 
 
 class TestScenePerlin : public Scene {
-
     using Scene::Scene;
-    
     Minimap* minimap;
-
     int ticks;
-    int seed = 0;
 
     public:
 
     virtual void prepare() {
-
         minimap = new Minimap(renderer, 512, 512, 1);
         objects.push_back(minimap);
-
     }
 
-    // void update(State* state) {
-    //     if (SDL_GetTicks()-ticks>500) {
-    //         int alpha;
-    //         srand(clock());
-    //         //float seed = rand();
-    //         seed++;
-    //         float step = 0.333;
-    //         for (int y=0; y<100; y++) {
-    //             for (int x=0; x<100; x++)
-    //             {
-    //                 // alpha = (noise((float)x*step+seed, (float)y*step+seed, 1.1) + 1) / 2.0  * 255.0;
-    //                 alpha = (noise((float)x*step+seed, y, 1.1) + 1) / 2.0  * 255.0;
-    //                 minimap->setPixel(x, y, 255, 255, 255, alpha);
-    //             }
-
-    //         }
-    //         ticks = SDL_GetTicks();
-    //     }
-    // }
-
     void update(State* state) {
-        if (SDL_GetTicks()-ticks>100) {
-            int alpha;
+        if (SDL_GetTicks()-ticks>1000) {
             srand(clock());
-            OpenSimplexNoise::Noise noise{rand()}; // Init
-            for (int y=0; y<512; y++) {
-                for (int x=0; x<512; x++)
-                {
-                    alpha = (noise.eval(x*0.01, y*0.01) + 1) / 2.0  * 255.0; // Use
-                    minimap->setPixel(x, y, 255, 255, 255, alpha);
-                }
-
-            }
+            minimap->generateTerrain(rand(), 0.01, 6, TERRAIN_RANGES, TERRAIN_COLORS);
             ticks = SDL_GetTicks();
         }
     }
